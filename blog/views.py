6893ddc1974a6e895af234c1c7bc39c8c1a548ff
Nischal Moktan <nischal123321@gmail.com>
2025-07-23 08:53:03 +0545

    commited

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
 
 def blog_list(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        extra_details = request.POST.get('extra_details')
+        image = request.FILES.get('image')
+
+        if title and content:
+            slug = slugify(title)
+            post = Post.objects.create(
+                title=title,
+                slug=slug,
+                content=content,
+                extra_details=extra_details,
+                image=image,
+                author=request.user
+            )
+            return redirect('blog_list')
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/blog_list.html', {'posts': posts})
 
